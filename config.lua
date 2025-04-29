5590a69a2c8e2ebe690270b53347f8f3b3a1ca54
LG <158826906+lundgreen01@users.noreply.github.com>
2025-04-29 17:47:15 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {
+    Background = "#2a3535",
+    Line = "#52a5ab"
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+game "gta5"
+
+shared_scripts {
+    "config.lua"
+}
+
+client_scripts {
+    "client/main.lua"
+}
+
+files {
+  "html/index.html",
+  "html/assets/*.js",
+  "html/assets/*.css"
+}
+
+ui_page "html/index.html"
